3c483a63a837d23d778bf738e82df7db522bcc34
kleber-ar <kleber-ar@hotmail.com>
2025-09-09 19:25:40 -0300

    feat(Header): Implement total calculation logic

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,19 @@ import type { RootState } from "../redux/reducers";
 function Header() {
   const email = useSelector((state: RootState) => state.user.email);
   const expenses = useSelector((state: RootState) => state.wallet.expenses);
-  const total = 0;
+
+  const total = expenses.reduce((acc, expense) => {
+    const value = Number(expense.value);
+    const currency = expense.currency;
+    const exchangeRate = Number(expense.exchangeRates[currency].ask);
+    return acc + value * exchangeRate;
+  }, 0);
+
 
   return (
     <header>
       <p>{email}</p>
-      <p>{total}</p>
+      <p>{total.toFixed(2)}</p>
       <p>BRL</p>
     </header>
   );
