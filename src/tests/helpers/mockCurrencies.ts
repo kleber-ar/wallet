f33ba1a712281f8923414ba2e7ab63530da15d39
kleber-ar <kleber-ar@hotmail.com>
2025-09-15 03:28:03 -0300

    test(mock): create mock currencies

---
+++ src/tests/helpers/mockCurrencies.ts
@@ -0,0 +1,7 @@
+const mockCurrencies = {
+  USD: { code: 'USD', ask: '5.00', name: 'Dólar Americano/Real Brasileiro' },
+  EUR: { code: 'EUR', ask: '6.00', name: 'Euro/Real Brasileiro' },
+  BRL: { code: 'BRL', ask: '1.00', name: 'Real Brasileiro/Real Brasileiro' },
+};
+
+export default mockCurrencies;
